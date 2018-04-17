dfd79d7e43e3c7989bd5a22cafc75c43b654b8ed
Maria Frascella <mfrascella16@gmail.com>
2018-04-17 12:29:38 -0400

    Initial commit

---
+++ Prog2ProjectTravelingSaleman/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+
+namespace Prog2ProjectTravelingSaleman
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            TestSolution anArray = new TestSolution();
+            char[] points = { 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j' };
+            Console.WriteLine(points);
+            Console.ReadLine();
+            Console.WriteLine(anArray.Shuffle(points));
+            Console.WriteLine(anArray.Distance(points));
+            
+
+
+            Console.ReadLine();
+
+
+
+
+
+        }
+    }
+}
---
+++ Prog2ProjectTravelingSaleman/TestSolution.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+
+namespace Prog2ProjectTravelingSaleman
+{
+    public class TestSolution
+    {
+        //array of chars
+        private static Random rng = new Random();
+        private char[] points = {'a', 'b', 'c', 'e', 'f', 'g', 'h', 'i', 'j'}; //ascii 97-106
+        
+        
+
+        //values shuffle from housing
+        public char[] Shuffle(char[] anArray)
+        {
+            int counter = anArray.Length - 1;
+
+            while (counter > 2)
+            {
+                counter--;
+                int index = rng.Next(1, counter);
+                char value = anArray[index];
+                anArray[index] = anArray[counter];
+                anArray[counter] = value;
+            }
+
+            return anArray;
+        }
+        
+
+
+        //reads the position of the array and subtracts the ascii character*
+        //need to add it to the score somehow
+        public double Distance(char[] points)
+        {
+            double score = 0; //total distanced traveled
+            int counter = 0;
+            double distance = 0;
+           
+
+            while (counter < points.Length - 1)
+            {
+                //Console.WriteLine(points[counter]);
+                //Console.ReadLine();
+
+                distance = Math.Abs(points[counter] - points[counter + 1]);
+                score += distance;
+
+
+                counter++;
+
+            }
+
+
+            return score;
+        }
+        
+
+
+
+
+
+        //use ascii character vaules
+        //randomize point generation
+        //rank calculated point distances by smallest to greatest
+        //start to mix up top points
+
+
+        public TestSolution(int x)
+        {
+
+        }
+
+        
+        public TestSolution()
+        {
+            this.points = this.Shuffle(points);
+            
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
